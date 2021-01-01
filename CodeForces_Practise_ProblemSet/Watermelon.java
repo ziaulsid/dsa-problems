7f1de8d8285aa2e7e216ab48b7b2cfe6f9422de3
ziaulsiddiqui <ziauls42@gmail.com>
2021-01-01 22:36:56 +0530

    CF practice problems

---
+++ CodeForces_Practise_ProblemSet/Watermelon.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Watermelon {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int weight = scanner.nextInt();
+
+        if(weight > 2 && weight % 2 == 0) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
