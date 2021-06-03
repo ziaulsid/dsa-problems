3aa75e685f55f4700a4d1e3fec0b638e733b2f38
Zia-ul-haq Siddiqui <ziauls42@gmail.com>
2021-06-04 02:45:52 +0530

    Randomized Set solution.
    Randomized Set with O(1) average case complexity using HashMap and ArrayList. HashSet works fine but the remove method will get executed in linear time complexity. I have used random.nextInt() method which will give equal chance to all the elements.

---
+++ Design/RandamizedSet.java
@@ -0,0 +1,52 @@
+class RandomizedSet {
+
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    Random rand = new Random();
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(map.containsKey(val)) return false;
+        
+        map.put(val, list.size());
+        list.add(val);
+        
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) return false;
+        
+        int lastElement = list.get(list.size() - 1);
+        int idxDeleteElement = map.get(val);
+        
+        
+        list.set(idxDeleteElement, lastElement);
+        map.put(lastElement, idxDeleteElement);
+        
+        list.remove(list.size() - 1);
+        map.remove(val);
+        
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+        
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
